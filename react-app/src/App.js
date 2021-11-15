f64242c26e2c66a0d66741c6a2d93dff1683a673
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-11-15 17:38:46 +0900

    검색바 구현중

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -6,13 +6,14 @@ import {
 import React from "react";
 import Home from "./pages/Home";
 import TopNav from "./conponents/TopNav";
+import SearchBar from "./conponents/SearchBar";
 import './conponents/common.css';
 
 function App() {
   return (
     <>
       <TopNav />
-
+      <SearchBar />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
--- react-app/src/conponents/SearchBar.js
+++ react-app/src/conponents/SearchBar.js
@@ -1,1 +1,18 @@
 import React from "react";
+import './SearchBar.css';
+import { Link, BrowserRouter } from "react-router-dom";
+
+function SearchBar() {
+    return (
+        <div className="search-bar">
+            <BrowserRouter>
+                <Link to="/">
+                    <img src="assets/logo/logo.png" alt="title logo" className="logo" />
+                </Link>
+            </BrowserRouter>
+            <input className="searching" placeholder="음식점 또는 관광지를 검색하세요." maxLength="50" />
+        </div>
+    );
+}
+
+export default SearchBar;
