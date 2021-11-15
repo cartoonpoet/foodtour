47976c62190b2da6c2a9e313b4242aa4b4ba72e7
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-11-15 18:15:44 +0900

    검색창 구현

--- react-app/src/conponents/SearchBar.js
+++ react-app/src/conponents/SearchBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './SearchBar.css';
 import { Link, BrowserRouter } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
 function SearchBar() {
     return (
@@ -11,6 +12,7 @@ function SearchBar() {
                 </Link>
             </BrowserRouter>
             <input className="searching" placeholder="음식점 또는 관광지를 검색하세요." maxLength="50" />
+            <button className="search-btn"><BsSearch /></button>
         </div>
     );
 }
