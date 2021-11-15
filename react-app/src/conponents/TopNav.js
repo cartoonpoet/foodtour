87c7a65d48b4fa1d70e3af7659027464959359d3
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-11-15 17:01:30 +0900

    카카오 로그인 sdk 추가

--- react-app/src/conponents/TopNav.js
+++ react-app/src/conponents/TopNav.js
@@ -4,6 +4,7 @@ import { AiFillHome } from "react-icons/ai";
 import { Link, BrowserRouter } from "react-router-dom";
 
 
+// 즐겨찾기
 function bookmark_add() {
     let bookmark_url = "도메인입력";
     let bookmark_name = "홈페이지 타이틀";
@@ -14,6 +15,19 @@ function bookmark_add() {
         alert('이 브라우저는 즐겨찾기 추가 기능을 지원하지 않습니다.');
         return false;
     }
+}
+
+// 카카오 로그인
+const { Kakao } = window;
+function loginWithKakao() {
+    Kakao.Auth.login({
+        success: function (authObj) {
+            alert(JSON.stringify(authObj))
+        },
+        fail: function (err) {
+            alert(JSON.stringify(err))
+        },
+    })
 }
 
 function TopNav() {
