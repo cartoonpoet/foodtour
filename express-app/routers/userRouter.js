c3340e0eb6b88808e7986ab1e8df16298fa6e55c
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-11-15 11:59:45 +0900

    eslint, prettier 설정

--- express-app/routers/userRouter.js
+++ express-app/routers/userRouter.js
@@ -3,13 +3,17 @@ import con from '../mysql';
 
 const userRouter = express.Router();
 
-userRouter.get('/user', function(req, res){
+userRouter.get('/user', function (req, res) {
     const sql = "SELECT * FROM social_type"
     con.query(sql, function (err, result, fields) {
         if (err) throw err;
         res.send(result)
     });
 });
+
+userRouter.post('/user', function (req, res) {
+
+})
 
 
 export default userRouter;
