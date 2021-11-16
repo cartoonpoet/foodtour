2ff0ba2a7730c0e6e06c2a54c71d780b3f6f8e42
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-11-16 14:08:07 +0900

    검색 유효성 체크

--- react-app/src/conponents/SearchBar.js
+++ react-app/src/conponents/SearchBar.js
@@ -10,6 +10,14 @@ function SearchBar() {
         setKeyword(e.target.value);
     }
 
+    const onSearch = (e) => {
+        if (keyword.length <= 0) {
+            alert('최소 한자이상의 키워드를 입력해야 합니다.');
+            e.preventDefault();
+            return;
+        }
+    }
+
     return (
         <div className="search-bar">
             <BrowserRouter>
@@ -19,7 +27,7 @@ function SearchBar() {
             </BrowserRouter>
             <input className="searching" placeholder="음식점 또는 관광지를 검색하세요." maxLength="50" onChange={onChange} value={keyword} />
             <BrowserRouter>
-                <Link to={`/search/${keyword}`} className="search-btn">
+                <Link to={`/search/${keyword}`} className="search-btn" onClick={onSearch}>
                     <BsSearch />
                 </Link>
             </BrowserRouter>
