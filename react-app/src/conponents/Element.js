5385b2adfa7d6636149be894f3b66153595a3df7
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-11-17 09:15:36 +0900

    검색 결과 요소 컴포넌트 구현

--- react-app/src/conponents/Element.js
+++ react-app/src/conponents/Element.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Element.css";
 import { BsFillEyeFill, BsFillPencilFill } from "react-icons/bs";
-
+import { Link } from "react-router-dom";
 
 function Element() {
     return (
         <>
             <div className="element-container">
-                <img class="thumb" alt="img is not" src="https://mp-seoul-image-production-s3.mangoplate.com/513273_1598598343472200.jpg?fit=around|359:240&crop=359:240;*,*&output-format=jpg&output-quality=80" />
+                <Link to="/">
+                    <img className="thumb" alt="img is not" src="https://mp-seoul-image-production-s3.mangoplate.com/513273_1598598343472200.jpg?fit=around|359:240&crop=359:240;*,*&output-format=jpg&output-quality=80" />
+                </Link>
                 <ul class="info-box">
-                    <li class="name-canvas"><span className="name">스시ddsdfsdfsdfsdfdsfsdffsdffsdfsdffds호시카이</span> <span class="grade">4.7</span></li>
-                    <li>음식점</li>
-                    <li>
-                        <BsFillEyeFill />&nbsp;1,000
-                        <BsFillPencilFill />&nbsp;245
+                    <li className="name-canvas"><Link to="/" className="name">스시ddsdfsdfsdfsdfdsfsdffsdffsdfsdffds호시카이</Link> <span className="grade">4.7</span></li>
+                    <li className="fliter-type">음식점</li>
+                    <li className="extra-info">
+                        <BsFillEyeFill className="views" />1,000
+                        <BsFillPencilFill className="reviews" />245
                     </li>
                 </ul>
             </div>
