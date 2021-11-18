d200cdff7bddf704102956b5dc260abcba3f4185
JunHo SON <38898759+cartoonpoet@users.noreply.github.com>
2021-11-18 11:26:59 +0900

    이름, 타입, 조회수, 리뷰수 설계 구현

--- react-app/src/pages/DetailInfo.js
+++ react-app/src/pages/DetailInfo.js
@@ -8,6 +8,7 @@ import "swiper/components/pagination/pagination.scss";
 import SwiperCore, {
     Pagination, Navigation
 } from 'swiper';
+import { BsFillEyeFill, BsFillPencilFill } from "react-icons/bs";
 
 
 SwiperCore.use([Pagination, Navigation]);
@@ -19,6 +20,18 @@ function DetailInfo({ location }) {
     return (
         <>
             <hr className="top-line" />
+
+            <ul className="title-box">
+                <li className="type">관광지</li>
+                <li className="title">월드컵공원</li>
+                <li className="extra-info-box">
+                    <BsFillEyeFill />
+                    <span className="views">117,495</span>
+                    <BsFillPencilFill />
+                    <span className="reviews">41</span>
+                </li>
+            </ul>
+
             <Swiper slidesPerView={5} spaceBetween={10} slidesPerGroup={1} loop={true} loopFillGroupWithBlank={true} pagination={{
                 "clickable": true
             }} navigation={true} className="mySwiper">
@@ -32,6 +45,10 @@ function DetailInfo({ location }) {
                 <SwiperSlide><img src="http://tong.visitkorea.or.kr/cms/resource/38/2680838_image2_1.jpg" alt="none" className="swiper-img-size-fix" /></SwiperSlide>
                 <SwiperSlide><img src="http://tong.visitkorea.or.kr/cms/resource/35/2680835_image2_1.jpg" alt="none" className="swiper-img-size-fix" /></SwiperSlide>
             </Swiper>
+
+            <div className="detail-info-container">
+
+            </div>
         </>
     );
 }
